52a5a0c041342f4baae0bd70f8d5ce800841121f
Romain Liautaud <romain.liautaud@ens-lyon.fr>
2018-02-04 02:43:14 +0100

    Simplified Webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -2,63 +2,50 @@ const path = require('path')
 const webpack = require('webpack')
 const extract = require('extract-text-webpack-plugin')
 
-const isDev = (process.env.NODE_ENV === 'development')
-
-
-const extractSass = new extract({
-    filename: '[name].css',
-    disable: isDev
-})
-
-const cssLoader = {
-  loader: 'css-loader',
-  options: {
-    minimize: !isDev,
-    sourceMap: isDev
-  }
-}
-
+// Custom loader for static assets
 const fileLoader = {
   loader: 'file-loader',
   options: {
-    name: '[path][name].[ext]'
+    name: '[path][name].[ext]',
+    context: path.resolve(__dirname, 'assets')
   }  
 }
 
-
+// Webpack configuration
 module.exports = {
   entry: [
-    './src/index.js', 
+    './src/index.js',
     './assets/sass/style.scss',
     'multi-entry-loader?include=./assets/img/**.*!',
     'multi-entry-loader?include=./assets/fonts/**.*!',
   ],
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+
+  // Extract the CSS files from 
   plugins: [
     new extract('bundle.css'),
-    new webpack.optimize.UglifyJsPlugin({minimize: !isDev})
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: extract.extract([cssLoader]),
+        use: extract.extract(['css-loader']),
       },
       {
         test: /\.scss$/,
         use: extract.extract({
           fallback: 'vue-style-loader',
-          use: [cssLoader, 'sass-loader'],
+          use: ['css-loader', 'sass-loader'],
         })
       },
       {
         test: /\.sass$/,
         use: extract.extract({
           fallback: 'vue-style-loader',
-          use: [cssLoader, 'sass-loader?indentedSyntax'],
+          use: ['css-loader', 'sass-loader?indentedSyntax'],
         })
       },
       {
@@ -66,8 +53,14 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           loaders: {
-            'scss': ['vue-style-loader', cssLoader, 'sass-loader'],
-            'sass': ['vue-style-loader', cssLoader, 'sass-loader?indentedSyntax']
+            'scss': extract.extract({
+              fallback: 'vue-style-loader',
+              use: ['css-loader', 'sass-loader'],
+            }),
+            'sass': extract.extract({
+              fallback: 'vue-style-loader',
+              use: ['css-loader', 'sass-loader?indentedSyntax'],
+            })
           }
         }
       },
